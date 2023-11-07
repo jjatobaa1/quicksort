f60899c55b87b6a2aa363c5bcabb37d2013af805
João Jatobá <92325974+jjatobaa1@users.noreply.github.com>
2023-11-07 09:49:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const unsortedArray = [5, 2, 9, 3, 6, 1, 8, 4, 7];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray);
